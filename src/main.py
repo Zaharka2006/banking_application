36a0d6c0843e7395a01048a7436968f8d198c848
PythonGod <3935420@gmail.com>
2024-11-14 22:29:09 +0300

    error correction with linter Black PEP8

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from src import widget
 
-account_name = input('Введите название карты и её номера: ')
+account_name = input("Введите название карты и её номера: ")
 print(widget.mask_account_card(account_name))
 
-user_date = input('Введите вашу дату и время: ')
+user_date = input("Введите вашу дату и время: ")
 print(widget.get_date(user_date))
