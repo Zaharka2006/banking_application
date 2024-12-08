02a1b5a2527029798472523ac5e4a30c2c9cce3a
PythonGod <3935420@gmail.com>
2024-12-08 19:20:25 +0300

    bug fixes in module masks

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,7 @@
 def get_mask_card_number(number: str) -> str:
     """Функция, которая маскирует номер карты в формате 'XXXX XX** **** XXXX'"""
+    if not isinstance(number, str):
+        raise ValueError("Номер карты должен быть строкой, а не числом")
 
     if len(number) != 16 or not number.isdigit():
         raise ValueError("Номер карты должен содержать 16 цифр")
@@ -10,10 +12,12 @@ def get_mask_card_number(number: str) -> str:
 
 
 def get_mask_account(number: str) -> str:
-    """Функция, которая маскирует номер карты в формате '**XXXX'"""
+    """Функция, которая маскирует номер счёта в формате '**XXXX'"""
+    if not isinstance(number, str):
+        raise ValueError("Номер счёта должен быть строкой, а не числом")
 
     if len(number) != 20 or not number.isdigit():
-        raise ValueError("Номер карты должен содержать 20 цифр")
+        raise ValueError("Номер счёта должен содержать 20 цифр")
 
     mask_card_number = f"**{number[-4:]}"
 
